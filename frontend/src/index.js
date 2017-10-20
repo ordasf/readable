0fd9663cc46b0f63d73056a8576f4de83f39f4fa
Fernando Ordas <fernando.ordas@yapiko.com>
2017-10-20 22:33:15 +0200

    chore: Add compose to use the redux webtools with redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware }  from 'redux';
+import { createStore, applyMiddleware, compose }  from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducer from './reducers'
 import { BrowserRouter} from 'react-router-dom';
 
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  applyMiddleware(thunk));
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
