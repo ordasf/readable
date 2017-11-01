f889441c464e7611f7c23d365edef6c52faa59e0
Fernando Ordas <fernando.ordas@yapiko.com>
2017-11-01 20:23:38 +0100

    chore: Remove unused variable

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { withRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class CategoryList extends React.Component {
 
